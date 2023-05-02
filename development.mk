aa9f955571beab58a68f0cdc76aff16f6d5a43a5
tamakiii <tamakiii@users.noreply.github.com>
2023-05-02 18:19:34 +0900

    keep-profile-changes

--- development.mk
+++ development.mk
@@ -1,7 +1,10 @@
-.PHONY: help build run
+.PHONY: help setup build run
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
+
+setup: \
+	data/profile
 
 build:
 	npx --no -- nodemon --watch src --ext js,ts,html,css --exec 'make -f build.mk build'
@@ -13,6 +16,16 @@ run:
 		--target=chromium \
 		--browser-console \
 		--devtools \
-		--chromium-profile=web-ext.backup-chatgpt \
+		--chromium-profile=data/profile \
 		--profile-create-if-missing \
+		--keep-profile-changes \
 		--start-url=https://chat.openai.com/
+
+teardown:
+	rm -rf data/profile
+
+data/profile: data
+	mkdir $@
+
+data:
+	mkdir $@
