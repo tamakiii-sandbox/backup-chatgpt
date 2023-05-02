2d4d696603e2d3e8bb09adcf608659827e70f78a
tamakiii <tamakiii@users.noreply.github.com>
2023-05-02 20:45:37 +0900

    fixes

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,5 @@
 // src/background.ts
-import { Browser, browser } from "webextension-polyfill-ts";
+import { Browser, Tabs, browser } from "webextension-polyfill-ts";
 import * as Observe from './observe';
 
 export enum MessageType {
@@ -11,64 +11,31 @@ interface Message {
   payload?: any;
 }
 
-const getCurrentActiveTab = async (browser: Browser): Promise<browser.tabs.Tab | null> => {
+const getCurrentActiveTab = async (browser: Browser): Promise<Tabs.Tab | null> => {
   try {
     const tabs = await browser.tabs.query({ active: true, currentWindow: true });
-
     if (tabs.length > 0) {
-      const activeTab = tabs[0];
-      console.log("Current active tab:", activeTab);
-      return activeTab;
-    } else {
-      console.warn("No active tab found");
-      return null;
+      return tabs[0];
     }
   } catch (error) {
     console.error("Error getting current active tab:", error);
-    return null;
   }
+  return null;
 };
-
-async function injectContentScript(tabId: number): Promise<void> {
-  try {
-    // Check if the content script has already been injected
-    const results = await chrome.scripting.executeScript({
-      target: { tabId },
-      func: () => typeof (window as any).yourContentScriptMarker === 'undefined'
-    });
-
-    if (results.length === 0) {
-      throw new Error('Unable to get result from content script check');
-    }
-
-    if (results[0].result) {
-      // Inject the content script if it's not already injected
-      await chrome.scripting.executeScript({
-        target: { tabId, allFrames: true },
-        files: ['dist/content.js']
-      });
-    }
-  } catch (error) {
-    console.error('Error injecting content script:', error);
-  }
-}
 
 const saveChatData = async (message: Message) => {
   console.log('save chat data:', message)
   console.log(browser.tabs);
-  const currentActiveTab = await getCurrentActiveTab(browser);
-  if (currentActiveTab) {
+  const tab = await getCurrentActiveTab(browser);
+  if (tab && tab.id) {
     try {
-      await injectContentScript(currentActiveTab.id);
-      console.log('injected');
-
-      const response = await browser.tabs.sendMessage(currentActiveTab.id, {
+      const id = tab.id;
+      const response = await browser.tabs.sendMessage(id, {
         type: Observe.MessageType.GetDomElement,
       });
       console.log('response:', response);
     } catch (error) {
-      console.log('Caught error:', error);
-      console.log('currentActiveTab:', currentActiveTab);
+      console.warn('Caught error:', error);
     }
   }
 };
--- src/content.ts
+++ src/content.ts
@@ -1,9 +1,4 @@
 // src/content.ts
 import { observe } from './observe';
-// import { Runtime, browser } from 'webextension-polyfill-ts';
-
-// browser.runtime.onMessage.addListener((message: any, sender: Runtime.MessageSender) => {
-//   console.log('Observe: ', message, sender);
-// });
 
 observe('body');
