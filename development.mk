e32ba2c74d22629b3b64682eb7a46de908f48fae
tamakiii <tamakiii@users.noreply.github.com>
2023-05-02 16:51:04 +0900

    add run rule using web-ext, and use installed nodemon

--- development.mk
+++ development.mk
@@ -1,7 +1,17 @@
-.PHONY: help build
+.PHONY: help build run
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
 build:
-	npx --yes -- nodemon --watch src --ext js,ts,html,css --exec 'make -f build.mk build'
+	npx --no -- nodemon --watch src --ext js,ts,html,css --exec 'make -f build.mk build'
+
+run:
+	npx --no -- web-ext run \
+		--source-dir=. \
+		--watch-file='dist/**/*' \
+		--target=chromium \
+		--browser-console \
+		--chromium-profile=web-ext.backup-chatgpt \
+		--profile-create-if-missing \
+		--start-url=https://chat.openai.com/
